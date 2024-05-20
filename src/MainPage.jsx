07421ce3dd71b167d1efc66e188a7c15047456d7
Bryan K <2455350756@qq.com>
2024-05-20 13:38:09 +0800

    timeline looking changes

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -22,7 +22,7 @@ const MainPage = () => {
 
   
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px'}}>
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', width: '100%', height:'100%'}}>
       <div className="ICON">
         <img src="../picture/icon.jpg" alt="OneShot 🚀💫" />
       </div>
@@ -34,10 +34,10 @@ const MainPage = () => {
           </select>
           <UserTab />
         </div>
-        <div className="content">
+        <div className="Upload">
           <MediaPreviewer mediaType={selectedOption} />
         </div>
-        <div className="bottom">
+        <div className="Timeline">
           <TimelineEditor />
         </div>
         <button className="button" id="button" role="button" onClick={toggleTheme}>Light/Dark</button>
